a029a3e6e2f0ae99fdd5a32908f5645a77d95a2b
Lutz Hören <lutz.hoeren@me.com>
2023-05-10 17:55:22 +0200

    correctly copy aiMetadata objects in scene combiner

--- code/Common/SceneCombiner.cpp
+++ code/Common/SceneCombiner.cpp
@@ -1350,6 +1350,7 @@ void SceneCombiner::Copy(aiMetadata **_dest, const aiMetadata *src) {
             out.mData = new aiVector3D(*static_cast<aiVector3D *>(in.mData));
             break;
         case AI_AIMETADATA:
+            out.mData = new aiMetadata(*static_cast<aiMetadata *>(in.mData));
             break;
         default:
             ai_assert(false);
